1d222b073afb05cb0c05649a19d98eb453d4c431
subwurzburg <subwurzburg24@gmail.com>
2023-06-15 18:51:10 +0800

    test

--- _nuxt/static/1686647544/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FChe-Jieh\u002F_nuxt\u002Fstatic\u002F1686647544",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FChe-Jieh\u002F",assetsPath:"\u002FChe-Jieh\u002F_nuxt\u002F",cdnURL:a}}}}(null));
